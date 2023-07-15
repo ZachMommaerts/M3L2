8d248794124cdc2ef227e0378e8ff0db513c094b
ZachMommaerts <zmommaerts@gmail.com>
2023-07-14 22:59:24 -0500

    Initial Commit

---
+++ M3L2/M3L2App.swift
@@ -0,0 +1,17 @@
+//
+//  M3L2App.swift
+//  M3L2
+//
+//  Created by Zach Mommaerts on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct M3L2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
